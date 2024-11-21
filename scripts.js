6962642b6629a2fca3970df4f1accf767eaa332b
liam470 <ln1700@ttusd.org>
2024-11-21 10:02:03 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,174 +1,144 @@
-const canvas = document.getElementById("tetris");
-const context = canvas.getContext("2d");
+// Select the canvas and context
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
-const row = 20;
-const col = 10;
-const sq = 30; // Square size
-const empty = "#111"; // Empty color
+// Game settings
+const paddleWidth = 75;
+const paddleHeight = 10;
+const ballRadius = 8;
+const brickRowCount = 5;
+const brickColumnCount = 9;
+const brickWidth = 50;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
 
-// Draw square
-function drawSquare(x, y, color) {
-    context.fillStyle = color;
-    context.fillRect(x * sq, y * sq, sq, sq);
-    context.strokeStyle = "#222";
-    context.strokeRect(x * sq, y * sq, sq, sq);
-}
+let score = 0;
+let isGameOver = false;
 
-// Create board
-let board = [];
-for (let r = 0; r < row; r++) {
-    board[r] = [];
-    for (let c = 0; c < col; c++) {
-        board[r][c] = empty;
+// Paddle and Ball positions
+let paddleX = (canvas.width - paddleWidth) / 2;
+let ballX = canvas.width / 2;
+let ballY = canvas.height - 30;
+let ballDX = 2;
+let ballDY = -2;
+
+// Create the bricks
+let bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (let r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
 
-// Draw the board
-function drawBoard() {
-    for (let r = 0; r < row; r++) {
-        for (let c = 0; c < col; c++) {
-            drawSquare(c, r, board[r][c]);
-        }
+// Event listener for moving paddle
+document.addEventListener('mousemove', mouseMoveHandler, false);
+
+function mouseMoveHandler(e) {
+    const relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2;
     }
 }
 
-// Tetromino shapes
-const pieces = [
-    { shape: [[1, 1, 0], [0, 1, 1]], color: "red" }, // Z shape
-    { shape: [[0, 1, 1], [1, 1, 0]], color: "green" }, // S shape
-    { shape: [[0, 1, 0], [1, 1, 1]], color: "purple" }, // T shape
-    { shape: [[1, 1], [1, 1]], color: "yellow" }, // O shape
-    { shape: [[1, 0, 0], [1, 1, 1]], color: "orange" }, // L shape
-    { shape: [[0, 1, 0], [0, 1, 0], [0, 1, 1]], color: "cyan" }, // I shape
-    { shape: [[0, 0, 1], [1, 1, 1]], color: "blue" } // J shape
-];
-
-// Random piece
-function randomPiece() {
-    return pieces[Math.floor(Math.random() * pieces.length)];
+// Draw the paddle
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
 }
 
-// Piece constructor
-function Piece(tetromino) {
-    this.tetromino = tetromino.shape;
-    this.color = tetromino.color;
-    this.x = 3;
-    this.y = -2; // Start above the board
+// Draw the ball
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
 }
 
-// Draw piece
-Piece.prototype.draw = function () {
-    for (let r = 0; r < this.tetromino.length; r++) {
-        for (let c = 0; c < this.tetromino[r].length; c++) {
-            if (this.tetromino[r][c]) {
-                drawSquare(this.x + c, this.y + r, this.color);
+// Draw the bricks
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+                const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = '#0095DD';
+                ctx.fill();
+                ctx.closePath();
             }
         }
     }
-};
-
-// Undraw piece
-Piece.prototype.unDraw = function () {
-    for (let r = 0; r < this.tetromino.length; r++) {
-        for (let c = 0; c < this.tetromino[r].length; c++) {
-            if (this.tetromino[r][c]) {
-                drawSquare(this.x + c, this.y + r, empty);
-            }
-        }
-    }
-};
-
-// Check for collisions
-Piece.prototype.collision = function (x, y) {
-    for (let r = 0; r < this.tetromino.length; r++) {
-        for (let c = 0; c < this.tetromino[r].length; c++) {
-            if (!this.tetromino[r][c]) continue;
-            let newX = this.x + c + x;
-            let newY = this.y + r + y;
-            if (newX < 0 || newX >= col || newY >= row || (newY >= 0 && board[newY][newX] !== empty)) {
-                return true;
-            }
-        }
-    }
-    return false;
-};
-
-// Move down
-Piece.prototype.moveDown = function () {
-    if (!this.collision(0, 1)) {
-        this.unDraw();
-        this.y++;
-        this.draw();
-    } else {
-        this.lock();
-        p = new Piece(randomPiece());
-    }
-};
-
-// Move right
-Piece.prototype.moveRight = function () {
-    if (!this.collision(1, 0)) {
-        this.unDraw();
-        this.x++;
-        this.draw();
-    }
-};
-
-// Move left
-Piece.prototype.moveLeft = function () {
-    if (!this.collision(-1, 0)) {
-        this.unDraw();
-        this.x--;
-        this.draw();
-    }
-};
-
-// Lock piece
-Piece.prototype.lock = function () {
-    for (let r = 0; r < this.tetromino.length; r++) {
-        for (let c = 0; c < this.tetromino[r].length; c++) {
-            if (this.tetromino[r][c]) {
-                board[this.y + r][this.x + c] = this.color;
-            }
-        }
-    }
-    // Check for completed rows
-    for (let r = row - 1; r >= 0; r--) {
-        let isFull = true;
-        for (let c = 0; c < col; c++) {
-            if (board[r][c] === empty) {
-                isFull = false;
-                break;
-            }
-        }
-        if (isFull) {
-            board.splice(r, 1);
-            board.unshift(new Array(col).fill(empty));
-        }
-    }
-};
-
-// Create the first piece
-let p = new Piece(randomPiece());
-p.draw();
-
-function drop() {
-    if (!gameOver) {
-        p.moveDown();
-    }
-    setTimeout(drop, 1000);
 }
 
-drop();
-
-document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowLeft") {
-        p.moveLeft();
-    } else if (event.key === "ArrowRight") {
-        p.moveRight();
-    } else if (event.key === "ArrowDown") {
-        p.moveDown();
-    } else if (event.key === "ArrowUp") {
-        p.rotate();
+// Detect collision with bricks
+function collisionDetection() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            const b = bricks[c][r];
+            if (b.status === 1) {
+                if (ballX > b.x && ballX < b.x + brickWidth && ballY > b.y && ballY < b.y + brickHeight) {
+                    ballDY = -ballDY;
+                    b.status = 0;
+                    score++;
+                    if (score === brickRowCount * brickColumnCount) {
+                        alert("You win, congratulations!");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
     }
-});
+}
+
+// Draw the score
+function drawScore() {
+    document.getElementById('score').innerText = score;
+}
+
+// Draw everything
+function draw() {
+    if (isGameOver) {
+        alert("Game Over");
+        document.location.reload();
+        return;
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+    drawBall();
+    drawPaddle();
+    drawScore();
+    collisionDetection();
+
+    if (ballX + ballDX > canvas.width - ballRadius || ballX + ballDX < ballRadius) {
+        ballDX = -ballDX;
+    }
+    if (ballY + ballDY < ballRadius) {
+        ballDY = -ballDY;
+    } else if (ballY + ballDY > canvas.height - ballRadius) {
+        if (ballX > paddleX && ballX < paddleX + paddleWidth) {
+            ballDY = -ballDY;
+        } else {
+            isGameOver = true;
+        }
+    }
+
+    ballX += ballDX;
+    ballY += ballDY;
+
+    requestAnimationFrame(draw);
+}
+
+// Start the game
+draw();
